4d4001f87ac88dde4920290e2d3232dd022dfea2
Ziad Said <ziads3232@gmail.com>
2021-11-27 20:10:33 +0200

    add all files

---
+++ app/js/script.js
@@ -0,0 +1,252 @@
+const darkTheme = document.getElementById("dark");
+const lightTheme = document.getElementById("light");
+const violetTheme = document.getElementById("violet");
+const toggleButton = document.querySelector(".toggle__button");
+// get the prefer theme for the user
+const userPrefersDark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+const userPrefersLight =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches;
+
+// but the default theme
+if (userPrefersDark) {
+  darkTheme.checked = true;
+  toggleButton.style.right = "50px";
+}
+if (userPrefersLight) {
+  lightTheme.checked = true;
+  toggleButton.style.right = "28px";
+}
+
+// move the toggle button and change the colors
+const body = document.body;
+const buttons = document.querySelectorAll("button");
+const toggle = document.querySelector(".toggle__background");
+const diffButtons = document.querySelectorAll(".diff-color");
+const displayScreen = document.querySelector(".display");
+const equalButton = document.querySelector(".equals");
+const buttonsContainer = document.querySelector(".grid__container");
+
+// function we call it when to remove dark theme
+function removeDarkTheme() {
+  body.classList.remove("dark");
+  toggle.classList.remove("dark__toggle__background");
+  toggleButton.classList.remove("dark__toggle__button");
+  buttons.forEach((button) => {
+    button.classList.remove("dark__button");
+  });
+  diffButtons.forEach((diffButton) => {
+    diffButton.classList.remove("dark__diff");
+  });
+  equalButton.classList.remove("dark__equal");
+  displayScreen.classList.remove("dark__display");
+  buttonsContainer.classList.remove("dark__grid__container");
+}
+// function we call it when to remove violet theme
+function removeVioletTheme() {
+  body.classList.remove("violet");
+  toggle.classList.remove("violet__toggle__background");
+  toggleButton.classList.remove("violet__toggle__button");
+  buttons.forEach((button) => {
+    button.classList.remove("violet__button");
+  });
+  diffButtons.forEach((diffButton) => {
+    diffButton.classList.remove("violet__diff");
+  });
+  equalButton.classList.remove("violet__equal");
+  displayScreen.classList.remove("violet__display");
+  buttonsContainer.classList.remove("violet__grid__container");
+}
+
+// function we call it when to remove light theme
+function removeLightTheme() {
+  body.classList.remove("light");
+  toggle.classList.remove("light__toggle__background");
+  toggleButton.classList.remove("light__toggle__button");
+  buttons.forEach((button) => {
+    button.classList.remove("light__button");
+  });
+  diffButtons.forEach((diffButton) => {
+    diffButton.classList.remove("light__diff");
+  });
+  equalButton.classList.remove("light__equal");
+  displayScreen.classList.remove("light__display");
+  buttonsContainer.classList.remove("light__grid__container");
+}
+
+// light theme
+lightTheme.addEventListener("click", () => {
+  toggleButton.style.right = "28px";
+
+  // add light classes to change the colors
+  body.classList.add("light");
+  toggle.classList.add("light__toggle__background");
+  toggleButton.classList.add("light__toggle__button");
+  buttons.forEach((button) => {
+    button.classList.add("light__button");
+  });
+  diffButtons.forEach((diffButton) => {
+    diffButton.classList.add("light__diff");
+  });
+  equalButton.classList.add("light__equal");
+  displayScreen.classList.add("light__display");
+  buttonsContainer.classList.add("light__grid__container");
+
+  // remove all violet theme classes
+  removeVioletTheme();
+
+  // remove all dark theme classes
+  removeDarkTheme();
+});
+
+// dark theme
+darkTheme.addEventListener("click", () => {
+  toggleButton.style.right = "50px";
+
+  // add light classes to change the colors
+  body.classList.add("dark");
+  toggle.classList.add("dark__toggle__background");
+  toggleButton.classList.add("dark__toggle__button");
+  buttons.forEach((button) => {
+    button.classList.add("dark__button");
+  });
+  diffButtons.forEach((diffButton) => {
+    diffButton.classList.add("dark__diff");
+  });
+  equalButton.classList.add("dark__equal");
+  displayScreen.classList.add("dark__display");
+  buttonsContainer.classList.add("dark__grid__container");
+
+  // remove all light theme classes
+  removeLightTheme();
+
+  // remove all violet theme classes
+  removeVioletTheme();
+});
+
+// violet theme
+violetTheme.addEventListener("click", () => {
+  toggleButton.style.right = "5px";
+
+  // add violet classes to change the colors
+  body.classList.add("violet");
+  toggle.classList.add("violet__toggle__background");
+  toggleButton.classList.add("violet__toggle__button");
+  buttons.forEach((button) => {
+    button.classList.add("violet__button");
+  });
+  diffButtons.forEach((diffButton) => {
+    diffButton.classList.add("violet__diff");
+  });
+  equalButton.classList.add("violet__equal");
+  displayScreen.classList.add("violet__display");
+  buttonsContainer.classList.add("violet__grid__container");
+
+  // remove all light theme classes
+  removeLightTheme();
+
+  // remove all dark theme classes
+  removeDarkTheme();
+});
+
+// calculator
+const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".operation");
+const deleteButton = document.querySelector(".delete");
+const clearButton = document.querySelector(".clear");
+
+// clear all result from display screen
+clearButton.addEventListener("click", () => {
+  displayScreen.textContent = "";
+});
+
+// numbers buttons
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let value = e.target.textContent;
+
+    if (
+      (displayScreen.textContent.slice(-1) === "." && value === ".") ||
+      (displayScreen.textContent.slice(0) === "0" && value === "0")
+    )
+      return;
+
+    // if the first number equal 0 replace it with next number
+    if (displayScreen.textContent.slice(0) === "0") {
+      displayScreen.textContent = "";
+    }
+    displayScreen.textContent += value;
+  });
+});
+
+// operation buttons
+operationButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let value = e.target.textContent;
+
+    if (
+      displayScreen.textContent.slice(-1) === "-" ||
+      displayScreen.textContent.slice(-1) === "+" ||
+      displayScreen.textContent.slice(-1) === "/" ||
+      displayScreen.textContent.slice(-1) === "x"
+    ) {
+      let displayScreenContent = displayScreen.textContent;
+      let newContent = displayScreenContent.substring(
+        0,
+        displayScreenContent.length - 1
+      );
+      displayScreen.textContent = newContent;
+    }
+
+    // no operation in the beginning
+    if (displayScreen.textContent.length === 0 && value != "-") return;
+
+    // if there's previous operation
+    if (
+      displayScreen.textContent.includes("-") ||
+      displayScreen.textContent.includes("+") ||
+      displayScreen.textContent.includes("/") ||
+      displayScreen.textContent.includes("x")
+    ) {
+      const converted = displayScreen.textContent.replaceAll("x", "*");
+      const result = eval(converted);
+      displayScreen.textContent = "";
+
+      displayScreen.textContent += result;
+    }
+    displayScreen.textContent += value;
+  });
+});
+
+// delete last number
+deleteButton.addEventListener("click", () => {
+  let displayScreenContent = displayScreen.textContent;
+
+  let newContent = displayScreenContent.substring(
+    0,
+    displayScreenContent.length - 1
+  );
+  displayScreen.textContent = newContent;
+});
+
+// function to but comma after three digits
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// equal button
+equalButton.addEventListener("click", () => {
+  // convert x to * and display the result
+  if (displayScreen.textContent.includes("x")) {
+    const converted = displayScreen.textContent.replaceAll("x", "*");
+    const result = eval(converted);
+    const convertToEnNum = numberWithCommas(result);
+    displayScreen.textContent = convertToEnNum;
+  } else {
+    const result = eval(displayScreen.textContent);
+    const convertToEnNum = numberWithCommas(result);
+    displayScreen.textContent = convertToEnNum;
+  }
+});
